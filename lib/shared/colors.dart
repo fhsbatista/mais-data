edbec5e466c064ba4b60360c924a07018bdcd3c5
Fernando Batista <fernandohsbatista@gmail.com>
2020-06-11 11:43:08 -0300

    Updated accent color

--- lib/shared/colors.dart
+++ lib/shared/colors.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-const kPrimaryLightColor = Color(0xffa0a8ff);
 const kPrimaryColor = Colors.white;
 const kPrimaryDarkColor = Colors.white;
-const kAccentColor = Colors.amberAccent;
+const kAccentColor = Color(0xff6979f8);
