351ec276fd6dcb51e9d2a9e8cb16c2cdb91dbe49
Fernando Batista <fernandohsbatista@gmail.com>
2020-07-03 08:42:18 -0300

    Set a new theme for app bar actions

--- lib/main.dart
+++ lib/main.dart
@@ -5,11 +5,12 @@ import 'package:maisdata/shared/colors.dart';
 void main() => runApp(
       MaterialApp(
         theme: ThemeData(
-          fontFamily: 'SF UI Display',
-          primaryColor: kPrimaryColor,
-          primaryColorDark: kPrimaryDarkColor,
-          accentColor: kAccentColor,
-        ),
+            fontFamily: 'SF UI Display',
+            primaryColor: kPrimaryColor,
+            primaryColorDark: kPrimaryDarkColor,
+            accentColor: kAccentColor,
+            appBarTheme:
+                AppBarTheme(actionsIconTheme: IconThemeData(size: 30))),
         title: 'Mais Data',
         home: MainPage(),
       ),
